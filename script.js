865f80e819bad80a4d878e01ad1ba57eb9dcbd7a
Ritik <ritikraj2699@outlook.com>
2023-10-24 11:18:21 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,68 +1,41 @@
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-// import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+document.addEventListener('DOMContentLoaded', () => {
+  const dropArea = document.querySelector('.drop-area');
+  const uploadForm = document.getElementById('upload-form');
 
-document.addEventListener('DOMContentLoaded', function () {
-  const dropZone = document.getElementById('drop-zone');
-  const saveButton = document.getElementById('save-button');
-  const items = [];
-
-  const s3 = new S3Client({
-    region: 'ap-south-1', // Replace with your AWS region
-    credentials: {
-      accessKeyId: 'AKIAREAV3T6VHAGUIBGG',     // Replace with your AWS Access Key
-      secretAccessKey: 'X1tGvuqqIRNfZumBv0al9uqEFGhR6K', // Replace with your AWS Secret Access Key
-    },
+  dropArea.addEventListener('dragover', (e) => {
+      e.preventDefault();
+      dropArea.style.backgroundColor = '#e2f0f8';
   });
 
-  dropZone.addEventListener('dragover', function (e) {
-    e.preventDefault();
-    dropZone.classList.add('dragover');
+  dropArea.addEventListener('dragleave', () => {
+      dropArea.style.backgroundColor = '#fff';
   });
 
-  dropZone.addEventListener('dragleave', function () {
-    dropZone.classList.remove('dragover');
+  dropArea.addEventListener('drop', (e) => {
+      e.preventDefault();
+      const file = e.dataTransfer.files[0];
+      const fileInput = uploadForm.querySelector('input[type="file"]');
+      fileInput.files = e.dataTransfer.files;
+      dropArea.style.backgroundColor = '#fff';
+      console.log(e);
+      console.log("heueueueuue");
+      console.log(file);
   });
 
-  dropZone.addEventListener('drop', function (e) {
-    e.preventDefault();
-    dropZone.classList.remove('dragover');
+  uploadForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const formData = new FormData(uploadForm);
 
-    const itemText = e.dataTransfer.getData('text/plain');
-    items.push(itemText);
-
-    const itemElement = document.createElement('div');
-    itemElement.textContent = itemText;
-    dropZone.appendChild(itemElement);
-  });
-
-  saveButton.addEventListener('click', function () {
-    if (items.length === 0) {
-      alert('No items to save.');
-      return;
-    }
-
-    const combinedText = items.join('\n');
-
-
-    const username = 'ritik';  // Replace with the actual username
-    const timestamp = new Date().toISOString().replace(/[:.]/g, '-'); // Converts timestamp to a valid S3 key
-    const s3Key = `user-files/${username}/${timestamp}-textfile.txt`;
-
-    // Save the combined text as a text file in AWS S3
-    const params = {
-      Bucket: 'ritik26',     // Replace with your AWS S3 bucket name
-      Key: s3Key,      // Specify the S3 key
-      Body: combinedText,
-      ACL: 'public-read',             // Optional: Set access control
-    };
-
-    s3.send(new PutObjectCommand(params))
-      .then(() => {
-        alert('Text file saved to S3 successfully');
+      fetch('/upload', {
+          method: 'POST',
+          body: formData
       })
-      .catch((error) => {
-        console.error('S3 upload error:', error);
-        alert('Error saving text file to S3');
+      .then(response => response.text())
+      .then(data => {
+          alert(data);
+      })
+      .catch(error => {
+          console.error('Error:', error);
       });
   });
 });
--- server.js
+++ server.js
@@ -1,53 +1,55 @@
 const express = require('express');
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-const bodyParser = require('body-parser');
+const multer = require('multer');
+const AWS = require('aws-sdk');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.text());
+app.use(express.static(__dirname));
 
-// AWS S3 Configuration
-const s3 = new S3Client({
-  region: 'ap-south-1', // Replace with your AWS region
-  credentials: {
-    accessKeyId: 'AKIAREAV3T6VHAGUIBGG',     // Replace with your AWS Access Key
-    secretAccessKey: 'X1tGvuqqIRNfZumBv0al9uqEFGhR6K', // Replace with your AWS Secret Access Key
-  },
+// Configure AWS SDK with your credentials
+AWS.config.update({
+  accessKeyId: 'AKIAREAV3T6VA5NINTWV',
+  secretAccessKey: 'HKUqBzHgPH0eOagjPmDkULHT6F8ifoYubalAuxoE',
+  region: 'ap-south-1',
 });
 
-// Serve your HTML/CSS/JS files
-app.use(express.static(__dirname));
-console.log(__dirname);
+// const s3 = new S3Client({
+//   region: 'ap-south-1', // Replace with your AWS region
+//   credentials: {
+//     accessKeyId: 'AKIAREAV3T6VHAGUIBGG',     // Replace with your AWS Access Key
+//     secretAccessKey: 'X1tGvuqqIRNfZumBv0al9uqEFGhR6K', // Replace with your AWS Secret Access Key
+//   },
+// });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
+const s3 = new AWS.S3();
 
-const username = 'ritik';  // Replace with the actual username
-const timestamp = new Date().toISOString().replace(/[:.]/g, '-'); // Converts timestamp to a valid S3 key
-const s3Key = `user-files/${username}/${timestamp}-textfile.txt`;
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-// Save combined items as a text file in AWS S3
-app.post('/save', (req, res) => {
-  const combinedText = req.body;
+app.post('/upload', upload.single('file'), (req, res) => {
+  const { file } = req;
+  const combinationData = req.body.combinationData;
 
-  // Save the combined text as a text file in AWS S3
+  // Create a text file from the combination data
+  const textData = `Combination Data:\n${combinationData}`;
+
+  // Define S3 parameters
   const params = {
-    Bucket: 'ritik26',     // Replace with your AWS S3 bucket name
-    Key: s3Key,      // Specify the S3 key
-    Body: combinedText,
-    ACL: 'public-read',             // Optional: Set access control
+    Bucket: 'ritik26',
+    Key: 'combination.txt',
+    Body: textData,
   };
 
-  s3.send(new PutObjectCommand(params))
-    .then(() => {
-      res.json({ message: 'Text file saved to S3 successfully' });
-    })
-    .catch((error) => {
-      console.error('S3 upload error:', error);
-      res.status(500).json({ message: 'Error saving text file to S3' });
-    });
+  // Upload the text file to S3
+  s3.upload(params, (err, data) => {
+    if (err) {
+      console.error('Error uploading to S3:', err);
+      return res.status(500).send('Error uploading to S3');
+    }
+    res.send('File uploaded to S3 successfully');
+  });
 });
 
 app.listen(port, () => {
