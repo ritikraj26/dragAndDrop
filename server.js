57e0af094ec701b9ef8f6829c77e65f644267e29
Ritik <ritikraj2699@outlook.com>
2023-10-25 20:08:39 +0530

    modified server.js

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
 const express = require('express');
 const multer = require('multer');
 const AWS = require('aws-sdk');
-const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.static(__dirname));
 
-// Configure AWS SDK with your credentials
 AWS.config.update({
   accessKeyId: 'AKIAREAV3T6VA5NINTWV',
   secretAccessKey: 'HKUqBzHgPH0eOagjPmDkULHT6F8ifoYubalAuxoE',
   region: 'ap-south-1',
 });
-
-// const s3 = new S3Client({
-//   region: 'ap-south-1', // Replace with your AWS region
-//   credentials: {
-//     accessKeyId: 'AKIAREAV3T6VHAGUIBGG',     // Replace with your AWS Access Key
-//     secretAccessKey: 'X1tGvuqqIRNfZumBv0al9uqEFGhR6K', // Replace with your AWS Secret Access Key
-//   },
-// });
 
 const s3 = new AWS.S3();
 
@@ -31,18 +21,18 @@ const upload = multer({ storage: storage });
 app.post('/upload', upload.single('file'), (req, res) => {
   const { file } = req;
   const combinationData = req.body.combinationData;
+  // console.log(combinationData);
+  const timestamp = Date.now();
+  const key = `combination-${timestamp}.txt`;
 
-  // Create a text file from the combination data
   const textData = `Combination Data:\n${combinationData}`;
 
-  // Define S3 parameters
   const params = {
     Bucket: 'ritik26',
-    Key: 'combination.txt',
+    Key: key,
     Body: textData,
   };
 
-  // Upload the text file to S3
   s3.upload(params, (err, data) => {
     if (err) {
       console.error('Error uploading to S3:', err);
